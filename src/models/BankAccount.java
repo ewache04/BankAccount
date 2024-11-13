463136ba4b25ea75d430b78c923c8faf3e10d4f4
Jeremiah E Ochepo <ochepojeremiahewache@gmail.com>
2024-11-12 23:08:19 -0500

    Create BankAccount.java

---
+++ src/models/BankAccount.java
@@ -0,0 +1,30 @@
+package models;
+
+// Abstract class representing a general Bank Account
+public abstract class BankAccount {
+    protected String accountNumber;  // Account number for identification
+    protected double balance;       // Current balance of the account
+
+    // Constructor to initialize the account number and balance
+    public BankAccount(String accountNumber, double balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    // Getter method to retrieve the account number
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    // Getter method to retrieve the account balance
+    public double getBalance() {
+        return balance;
+    }
+
+    // Abstract method for deposit operation, to be implemented in subclasses
+    public abstract void deposit(double amount);
+
+    // Abstract method for withdrawal operation, to be implemented in subclasses
+    public abstract void withdraw(double amount);
+}
+
